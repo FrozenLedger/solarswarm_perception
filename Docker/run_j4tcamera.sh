5f86a2085937fe3c1ee7d9030f7de0e2728f23e1
Denny Falls R. <82038149+FrozenLedger@users.noreply.github.com>
2024-12-07 21:44:17 +0100

    Update run_j4tcamera.sh
    Improved run file to remove container with the same name if any and give access to the camera. Using cgroup did not work.

--- Docker/run_j4tcamera.sh
+++ Docker/run_j4tcamera.sh
@@ -1,5 +1,7 @@
+docker stop jetson-j4tcamera
+docker rm jetson-j4tcamera
 docker run -it \
-    --name=jetson-j4tcamera-test \
+    --name=jetson-j4tcamera \
     --network=host \
     -v /home/$USER/Development/Volumes/noetic-pytorch/volume:/home/volume/ \
     --runtime nvidia \
@@ -8,6 +10,7 @@ docker run -it \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --privileged \
-    jetson/j4tcamera:latest
+    --restart unless-stopped \
+    solarswarm/j4tcamera:latest
 #    --device-cgroup-rule "c 81:* rmw" \
 #    --device-cgroup-rule "c 189:* rmw" \
