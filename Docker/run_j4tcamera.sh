2232d89e115b69186b634f99d4580933ab8dd75f
Denny Falls R. <82038149+FrozenLedger@users.noreply.github.com>
2024-12-07 22:40:31 +0100

    Update run_j4tcamera.sh
    Removed unused arguments from run script.

--- Docker/run_j4tcamera.sh
+++ Docker/run_j4tcamera.sh
@@ -3,14 +3,14 @@ docker rm jetson-j4tcamera
 docker run -it \
     --name=jetson-j4tcamera \
     --network=host \
-    -v /home/$USER/Development/Volumes/noetic-pytorch/volume:/home/volume/ \
+    -v /home/$USER/SolarSwarmAssets/:/home/SolarSwarmAssets/ \
     --runtime nvidia \
-    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --privileged \
     --restart unless-stopped \
     solarswarm/j4tcamera:latest
+#    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+#    --env="DISPLAY=$DISPLAY" \
+#    --env="QT_X11_NO_MITSHM=1" \
+#    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 #    --device-cgroup-rule "c 81:* rmw" \
 #    --device-cgroup-rule "c 189:* rmw" \
